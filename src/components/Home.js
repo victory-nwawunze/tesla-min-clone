ddbf2b517f7217d013e401e348cf001c651e8522
victory-nwawunze <victorynwawunze@gmail.com>
2022-03-29 07:05:41 -0700

    tesla

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,64 @@
 /** @format */
 import React from "react";
-
+import styled from "styled-components";
+import Section from "./Section";
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online For Touches Delivery"
+        backgroundImg="model-s.jpg"
+        rightBtnText="CUSTOM ORDER"
+        leftBtnText="EXISITING INVENTORY"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online For Touches Delivery"
+        backgroundImg="model-y.jpg"
+        rightBtnText="CUSTOM ORDER"
+        leftBtnText="EXISITING INVENTORY"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online For Touches Delivery"
+        backgroundImg="model-3.jpg"
+        rightBtnText="CUSTOM ORDER"
+        leftBtnText="EXISITING INVENTORY"
+      />
+      <Section
+        title="Model X"
+        description="Order Online For Touches Delivery"
+        backgroundImg="model-x.jpg"
+        rightBtnText="CUSTOM ORDER"
+        leftBtnText="EXISITING INVENTORY"
+      />
+      <Section
+        title="Little Cost Solar Panels In America"
+        description="Money-back guarantee"
+        backgroundImg="solar-roof.jpg"
+        rightBtnText="ORDER NOW"
+        leftBtnText="LEARN MORE"
+      />
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roof Cost Less Than A New Roof"
+        backgroundImg="solar-panel.jpg"
+        rightBtnText="ORDER NOW"
+        leftBtnText="LEARN MORE"
+      />
+      <Section
+        title="Accessories"
+        backgroundImg="accessories.jpg"
+        rightBtnText="ORDER NOW"
+      />
+    </Container>
+  );
 }
 
 export default Home;
+
+const Container = styled.div`
+  height: 100vh;
+  z-index: 10;
+`;
