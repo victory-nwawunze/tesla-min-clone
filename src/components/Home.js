20af96c50ab7d140874890fd2f0f9fece26d9d9c
victory-nwawunze <victorynwawunze@gmail.com>
2022-03-29 04:41:08 -0700

    tesla

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,71 +1,8 @@
 /** @format */
 import React from "react";
-import styled from "styled-components";
-import Section from "./Section";
+
 function Home() {
-  return (
-    <Container>
-      <Section
-        title="Model S"
-        description="Order Online For Touches Delivery"
-        backgroundImg="model-s.jpg"
-        leftBtnText="CUSTOM ORDER"
-        rightBtnText="EXISITING INVENTORY"
-      />
-      <Section
-        title="Model Y"
-        description="Order Online For Touches Delivery"
-        backgroundImg="model-y.jpg"
-        leftBtnText="CUSTOM ORDER"
-        rightBtnText="EXISITING INVENTORY"
-      />
-      <Section
-        title="Model 3"
-        description="Order Online For Touches Delivery"
-        backgroundImg="model-3.jpg"
-        leftBtnText="CUSTOM ORDER"
-        rightBtnText="EXISITING INVENTORY"
-      />
-      <Section
-        title="Model X"
-        description="Order Online For Touches Delivery"
-        backgroundImg="model-x.jpg"
-        leftBtnText="CUSTOM ORDER"
-        rightBtnText="EXISITING INVENTORY"
-      />
-      <Section
-        title="Model S"
-        description="Order Online For Touches Delivery"
-        backgroundImg="model-s.jpg"
-        leftBtnText="CUSTOM ORDER"
-        rightBtnText="EXISITING INVENTORY"
-      />
-      <Section
-        title="Little Cost Solar Panels In America"
-        description="Money-back guarantee"
-        backgroundImg="solar-panel.jpg"
-        leftBtnText="Order now"
-        rightBtnText="LEARN MORE"
-      />
-      <Section
-        title="Solar for New Roofs"
-        description="Solar Roof Cost Less Than A New Roof"
-        backgroundImg="solar-roof.jpg"
-        leftBtnText="Order now"
-        rightBtnText="LEARN MORE"
-      />
-      <Section
-        title="Accessories"
-        backgroundImg="accessories.jpg"
-        leftBtnText="SHOP NOW"
-      />
-    </Container>
-  );
+  return <div>Home</div>;
 }
 
 export default Home;
-
-const Container = styled.div`
-  height: 100vh;
-  z-index: 10;
-`;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,49 +1,8 @@
 /** @format */
-
 import React from "react";
-import styled from "styled-components";
-import Fade from "react-reveal/Fade";
-function Section({
-  title,
-  backgroundImg,
-  description,
-  leftBtnText,
-  rightBtnText,
-}) {
-  return (
-    <Wrap bgImage={backgroundImg}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
 
-      <Fade>
-        <Buttons>
-          <ButonGroup>
-            <RightButton>{rightBtnText}</RightButton>
-            <LeftButton>{leftBtnText}</LeftButton>
-          </ButonGroup>
-        </Buttons>
-      </Fade>
-    </Wrap>
-  );
+function Section() {
+  return <div>Section</div>;
 }
 
 export default Section;
-
-const Wrap = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: cover;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-image: ${(props) => `url("/images/${props.bgImage}")`};
-`;
-const ItemText = styled.div``;
-const Buttons = styled.div``;
-const ButonGroup = styled.div``;
-const LeftButton = styled.div``;
-const RightButton = styled(LeftButton)``;
